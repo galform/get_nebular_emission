7abeee3eb9f0b2a1922a11a5812e3d25556e415f
expox7 <75344115+expox7@users.noreply.github.com>
2023-02-15 12:50:11 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -4,6 +4,12 @@
 # list see the documentation:
 # http://www.sphinx-doc.org/en/master/config
 
+import mock
+ 
+MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot', 'scipy.interpolate']
+for mod_name in MOCK_MODULES:
+sys.modules[mod_name] = mock.Mock()
+
 # -- Path setup --------------------------------------------------------------
 
 # If extensions (or modules to document with autodoc) are in another directory,
